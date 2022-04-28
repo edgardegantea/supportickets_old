25ca86b4aa7062de9ab955106253947f62f066e1
edgardegantea <edgar.degante.a@gmail.com>
2022-04-28 15:31:42 -0500

    Actualización de vista show

--- app/Views/tickets/show.php
+++ app/Views/tickets/show.php
@@ -5,42 +5,52 @@
     La información mostrada corresponde al ticket de soporte
 
 	<div class="">
-
-
-
-
-
 		<!-- Main container -->
-<nav class="level">
-  <!-- Left side -->
-  <div class="level-left">
-    <div class="level-item">
-		<span class="tag is-info is-large">
-  		  <?php if ($ticket['status'] == 's01'): ?>
-  			  Estado: NO INICIADO
-  		  <?php elseif ($ticket['status'] == 's02'): ?>
-  			  Estado: INICIADO
-  		  <?php else: ?>
-  			  Estado: EN PROCESO
-  		  <?php endif; ?>
-  	  </span>
-    </div>
-    <div class="level-item">
-		<p class="is-size-1 has-text-link">
-  		<?= esc($ticket['title']); ?>
-  	  </p>
-    </div>
-  </div>
+		<nav class="level">
+		  <!-- Left side -->
+		  <div class="level-left">
+		    <div class="level-item">
 
-  <!-- Right side -->
-  <div class="level-right">
-    <p class="level-item"><a>Seguimiento</a></p>
-    <p class="level-item"><a class="button is-success">Contactar</a></p>
-  </div>
-</nav>
+		  		  <?php if ($ticket['status'] == 's01'): ?>
+					  <span class="tag is-light is-bordered is-large">
+		  			  	Estado: NO INICIADO
+					  </span>
+		  		  <?php elseif ($ticket['status'] == 's02'): ?>
+					  <span class="tag is-link is-large">
+		  			  	Estado: INICIADO
+					  </span>
+		  		  <?php else: ?>
+					  <span class="tag is-primary is-large">
+		  			  	Estado: EN PROCESO
+					  </span>
+		  		  <?php endif; ?>
+
+		    </div>
+		    <div class="level-item">
+				<p class="is-size-1 has-text-link">
+		  		<?= esc($ticket['title']); ?>
+		  	  </p>
+		    </div>
+		  </div>
+
+		  <!-- Right side -->
+		  <div class="level-right">
+		    <p class="level-item"><a>Seguimiento</a></p>
+		    <p class="level-item"><a class="button is-success">Contactar</a></p>
+		  </div>
+		</nav>
 
 	</div>
 
+	<div class="content">
+		Detalles:
+		<p class="mx-5 my-5 is-size-4 has-text-justify"><?= esc($ticket['description']); ?></p>
+	</div>
+
+	<div class="content">
+		Evidencia:
+		<p class="mx-5 my-5 is-size-5"><?= esc($ticket['evidence']); ?></p>
+	</div>
 
 
 
